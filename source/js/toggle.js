6036b90276ea4bbf71b94bfbb938e297893f780a
vkrassova <v.krassova@yandex.ru>
2020-08-23 21:33:53 +0300

    создает переключатель в page-header

---
+++ source/js/toggle.js
@@ -0,0 +1,14 @@
+var navMain = document.querySelector('.page-header');
+var navToggle = document.querySelector('.page-header__toggle');
+
+navMain.classList.remove('page-header--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('page-header--closed')) {
+    navMain.classList.remove('page-header--closed');
+    navMain.classList.add('page-header--opened');
+  } else {
+    navMain.classList.add('page-header--closed');
+    navMain.classList.remove('page-header--opened');
+  }
+})
